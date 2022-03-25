812f2d8b88794feea9a0ad656c40e947bc5aea5e
esanchezrojas <esanchez@Priscila.local>
2022-03-25 11:17:48 -0500

    Configuración loopback 4 y jsdoc

---
+++ src/index.js
@@ -0,0 +1,47 @@
+/**
+ * Inicio de proyect
+ * @module index
+ */
+
+
+/**
+ * This is a description of the MyClass constructor function.
+ * @class
+ * @classdesc esta es la descrip de mi class class.
+ */
+class entrada {
+
+}
+
+/**
+ * Carga el nombre del usuario logueado y lo muestra en la etiqueta con id perfil.
+ * @function perfil
+ * @param {object} user Trae los valores almacenados en la memoria cache.
+ */
+function Book(title, author) {}
+
+
+/**
+ * @typedef {string} NewType
+ */
+
+/**
+ * @function nueva 
+ * @param {NewType} a envia
+ * @param {number} e recibe
+ */
+function nueva(a, e) {
+
+}
+
+/**
+ * recibe la vocales
+ * @param {*} a 
+ * @param {*} e 
+ * @param {*} i 
+ * @param {*} o 
+ * @param {*} u 
+ */
+function nueva2(a, e, i, o, u) {
+
+}
